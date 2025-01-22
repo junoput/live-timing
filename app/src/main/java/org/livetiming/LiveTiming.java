216c0411b1e3fe8fdc882e053a5cda1c8ac728d9
junoput <junoput@outlook.com>
2025-01-22 20:50:45 +0100

    Added java files

---
+++ app/src/main/java/org/livetiming/LiveTiming.java
@@ -0,0 +1,5 @@
+package org.livetiming;
+
+public class LiveTiming {
+
+}
---
+++ app/src/main/java/org/livetiming/Main.java
@@ -0,0 +1,8 @@
+package org.livetiming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
