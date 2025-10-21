c228afff002920c4f8cdeb872c39cc8d8315f9a5
camentho <camentho@email.com>
2025-10-21 17:05:28 +0200

    Created model

---
+++ app/src/main/java/org/livetiming/model/CompetitorStatus.java
@@ -0,0 +1,5 @@
+package org.livetiming.model;
+
+public enum CompetitorStatus {
+    NOT_STARTED, ON_COURSE, FINISHED, DID_NOT_FINISH, DID_NOT_START, DISQUALIFIED, UNKNOWN
+}
---
+++ app/src/main/java/org/livetiming/model/Gender.java
@@ -0,0 +1,5 @@
+package org.livetiming.model;
+
+public enum Gender {
+    MALE, FEMALE, UNKNOWN
+}
