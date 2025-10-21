f23d3d23266bdf4bb8222aa195e34bddf0490a11
camentho <camentho@email.com>
2025-10-21 17:05:28 +0200

    Updated competitorList to devide by gender

--- app/src/main/java/org/livetiming/manager/CompetitorList.java
+++ app/src/main/java/org/livetiming/manager/CompetitorList.java
@@ -4,65 +4,76 @@ import org.livetiming.model.Category;
 import org.livetiming.model.CompetitorStatus;
 import org.livetiming.model.Gender;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class CompetitorList {
-    private final Map<Category, Set<Competitor>> competitorsByCategory;
+    private final Map<Category, Set<Competitor>> maleCompetitorsByCategory;
+    private final Map<Category, Set<Competitor>> femaleCompetitorsByCategory;
 
     public CompetitorList() {
-        this.competitorsByCategory = new HashMap<>();
+        this.maleCompetitorsByCategory = new HashMap<>();
+        this.femaleCompetitorsByCategory = new HashMap<>();
     }
 
     public CompetitorList(Set<Competitor> competitors) {
-        this.competitorsByCategory = new HashMap<>();
+        this();
         for (Competitor competitor : competitors) {
             this.addCompetitor(competitor);
         }
     }
 
     public void addCompetitor(Competitor competitor) {
+        Map<Category, Set<Competitor>> targetMap = getGendertMap(competitor.getGender());
         Category category = competitor.getCategory();
-        this.competitorsByCategory
-                .computeIfAbsent(category, k -> new HashSet<>())
-                .add(competitor);
+        targetMap.computeIfAbsent(category, k -> new HashSet<>()).add(competitor);
     }
 
     public void removeCompetitor(Competitor competitor) {
+        Map<Category, Set<Competitor>> targetMap = getGendertMap(competitor.getGender());
         Category category = competitor.getCategory();
-        Set<Competitor> competitors = this.competitorsByCategory.get(category);
+        Set<Competitor> competitors = targetMap.get(category);
         if (competitors != null) {
             competitors.remove(competitor);
             if (competitors.isEmpty()) {
-                this.competitorsByCategory.remove(category);
+                targetMap.remove(category);
             }
         }
     }
 
+    /**
+     * @return a Set of all competitors
+     */
     public Set<Competitor> getCompetitors() {
-        return this.competitorsByCategory.values().stream()
-                .flatMap(Set::stream)
-                .collect(Collectors.toSet());
+        Set<Competitor> competitors = getAllCompetitors(femaleCompetitorsByCategory);
+        competitors.addAll(getAllCompetitors(maleCompetitorsByCategory));
+        return competitors;
     }
 
+
     public Set<Competitor> getCompetitorsByCategory(Category category) {
-        return this.competitorsByCategory.getOrDefault(category, new HashSet<>());
+        Set<Competitor> competitors = new HashSet<>(maleCompetitorsByCategory.getOrDefault(category, new HashSet<>()));
+        competitors.addAll(femaleCompetitorsByCategory.getOrDefault(category, new HashSet<>()));
+        return competitors;
     }
 
     public Set<Competitor> getCompetitorsByStatus(CompetitorStatus status) {
-        return this.competitorsByCategory.values().stream()
-                .flatMap(Set::stream)
+        return getAllCompetitors(maleCompetitorsByCategory).stream()
                 .filter(competitor -> competitor.getStatus() == status)
                 .collect(Collectors.toSet());
     }
 
     public Set<Competitor> getCompetitorsByGender(Gender gender) {
-        return this.competitorsByCategory.values().stream()
+        return new HashSet<>(getAllCompetitors(getGendertMap(gender)));
+    }
+
+    private Map<Category, Set<Competitor>> getGendertMap(Gender gender) {
+        return gender == Gender.MALE ? maleCompetitorsByCategory : femaleCompetitorsByCategory;
+    }
+
+    private Set<Competitor> getAllCompetitors(Map<Category, Set<Competitor>> map) {
+        return map.values().stream()
                 .flatMap(Set::stream)
-                .filter(competitor -> competitor.getGender() == gender)
                 .collect(Collectors.toSet());
     }
 }
